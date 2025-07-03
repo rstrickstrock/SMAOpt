7bc62dce46039e03bee386f93336a801f795a354
Robo <62102557+rstrickstrock@users.noreply.github.com>
2025-07-03 15:33:11 +0200

    Delete FFLOW_TUTORIAL directory

--- FFLOW_TUTORIAL/energies/qmmm_collect.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-result_cwd=$1"/output/"
-
-cat $result_cwd"/Energy.rel.extrm.txt" | cut -f2 -d ' '
-
--- FFLOW_TUTORIAL/energies/qmmm_eval.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-eval_cwd=$1
-#echo "eval_cwd: $eval_cwd"
-
-cd $eval_cwd
-sbatch batch_slurm_eval_RCE.sh
